72afc36bd9b292d3646bbbec08444fb954004e18
mohamed khorchid <mohamed.khorchid.ocs@gmail.com>
2024-04-12 02:33:10 +0200

    fixed

--- assets/scripts.js/main.js
+++ assets/scripts.js/main.js
@@ -1,25 +1,3 @@
-//slides.nomImage = image1
-
-/*
-console.log(slides[0]) =>
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-
-
-console.log(slides[1]) =>
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-
-
-console.log(slides[1].image) => "slide2.jpg",
-*/
-
-//Méthode avec le tableau et l'incrémentation
-
 const slides = [
 	{
 		"image":"./assets/images/slideshow/slide1.jpg",
@@ -44,77 +22,84 @@ const arrowLeftDOM = document.querySelector(".arrow_left");
 const arrowRightDOM = document.querySelector(".arrow_right");
 const slideImageDOM = document.querySelector(".banner-img");
 const slideTextDOM = document.querySelector("#banner p");
+const dots = document.querySelector(".dots")
 let index = 0;
+
 
 // Fonctions
 function suivant() {
 	index++;
-	if (index > slides.length - 1) {/* slides.length pour traduire à javascript la dernière valeur du tableau pour qu'il revienne à la
-		 premire valeur sinon il prendra une 4eme valeur qui n'existe pas*/
-		index = 0;//retour à la première valeur
+
+	if (index > slides.length - 1) {
+		index = 0;
 	}
 
 	displaySlide()
+	updateDots()
 }
+
 
 /**
  * Afficher la slide précédente
  */
 function precedent() {
-	index--;/*je pars de l'image qui correspond à l'objet 0 du tableau et je décrémente, donc, je retire 1 et maintenant index = -1, 
-	sauf que javascript ne connait pas de valeur -1 dans le tableau il connait juste 0,1,2,3 
-	il faudra donc que index corresponde à autre chose que -1*/
-	if (index < 0) { //je crée une condition qui permet de vérifier si l'index correspond à un nombre inférieur au premier objet du tableau 
-		index = slides.length -1;/*si c'est le cas, je remplace la valeur -1 (qui est inférieur à 0 et que javascript ne connait pas)
-		 par la derniere valeur du tableau, c'est à dire, par l'objet 3*/
+	index--;
+	
+	if (index < 0) {
+		index = slides.length -1;
 	}
 
 	displaySlide()
+	updateDots()
 }
+
 
 /**
  * Remplace l'affichage de la slide en fonction de la valeur de l'index 
  */
 function displaySlide(){
-	slideImageDOM.src = slides[index].image;/* j'entengistre une nouvelle source à mon image qui prendra pour valeur la valeur image du tableau slide en fonction de la position de l'index*/
-	slideTextDOM.innerHTML=slides[index].tagLine; 
+	slideImageDOM.setAttribute("src", slides[index].image);
+	slideTextDOM.innerHTML = slides[index].tagLine;
 }
+
+
+/**
+ * Cette fonction créé un point pour chacun des objets du tableau slides et rempli le premier point
+ */
+function displayDots() {
+	slides.forEach((slide, index) => {
+		const dot = document.createElement("span")
+		dot.classList.add("dot")
+		dots.appendChild(dot)	
+
+		if (index === 0) {
+			dot.classList.add("dot_selected")
+		}
+	})
+}
+
+
+/**
+ * Cette fonction met à jour le remplissage des points en fonction de l'index
+ */
+function updateDots() {
+	const allDots = document.querySelectorAll(".dot");
+    
+    allDots.forEach((dot, dotPosition) => {
+        if (dotPosition === index) {
+            dot.classList.add("dot_selected");
+        } else {
+            dot.classList.remove("dot_selected");
+        }
+    });
+}
+
+
+// Appels des fonctions 
+displaySlide()
+displayDots()
 
 
 // Listeners
 arrowLeftDOM.addEventListener("click", precedent)
 arrowRightDOM.addEventListener("click", suivant)
-
-
-//Méthode en vérifiant les sources des images
-/*
-let myImage = document.querySelector(".banner-img");
-let myArrowLeft = document.querySelector(".arrow_left");
-let myArrowRight = document.querySelector(".arrow_right");
-
-myArrowRight.addEventListener("click", function() {
-	let mySrc = myImage.getAttribute("src");
-	if (mySrc === "./assets/images/slideshow/slide1.jpg") {
-		myImage.setAttribute("src", "./assets/images/slideshow/slide2.jpg")
-	} else if (mySrc === "./assets/images/slideshow/slide2.jpg") {
-		myImage.setAttribute("src", "./assets/images/slideshow/slide3.jpg")
-	} else if (mySrc === "./assets/images/slideshow/slide3.jpg") {
-		myImage.setAttribute("src", "./assets/images/slideshow/slide4.png")
-	} else {
-		myImage.setAttribute("src", "./assets/images/slideshow/slide1.jpg")
-	}
-})
-
-myArrowLeft.addEventListener("click", function() {
-	let mySrc = myImage.getAttribute("src");
-	if (mySrc === "./assets/images/slideshow/slide1.jpg") {
-		myImage.setAttribute("src", "./assets/images/slideshow/slide4.png")
-	} else if (mySrc === "./assets/images/slideshow/slide4.png") {
-		myImage.setAttribute("src", "./assets/images/slideshow/slide3.jpg")
-	} else if (mySrc === "./assets/images/slideshow/slide3.jpg") {
-		myImage.setAttribute("src", "./assets/images/slideshow/slide2.jpg")
-	} else {
-		myImage.setAttribute("src", "./assets/images/slideshow/slide1.jpg")
-	}
-})
-*/
